280eaaf71c8bddd5cc947fce63aeca77b6c943fa
CHIRAG JAIN <chirag.jain1@somaiya.edu>
2024-02-03 10:03:39 -0800

    Update interpret.py

--- picarx/interpret.py
+++ picarx/interpret.py
@@ -5,15 +5,11 @@ class Interpreter(object):
     def __init__(self,sensitivity=0.1,polarity=1):
         self.sensitivity = sensitivity * polarity
 
-    def processing(self, values, cal_values):
+    def processing(self, values):
         values = [x+1 if x == 0 else x for x in values]
         left, middle, right = values
 
         left, middle, right = values
-        left_cal, middle_cal, right_cal = cal_values
-        left = left/left_cal
-        middle = middle/middle_cal
-        right = right/right_cal
 
         if abs(right - left) > self.sensitivity: # means we're off the line
             if right > left:
