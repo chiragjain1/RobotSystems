6c72d00d65aa2d54f86b8cceccd570db9a763888
chirag jain <chirag.jain1@somaiya.edu>
2024-02-03 13:35:59 -0800

    concun first try

---
+++ picarx/concun_interpret.py
@@ -0,0 +1,36 @@
+import time
+from bus import Bus
+
+class Interpreter(object):
+    def __init__(self,sensitivity=0.1,polarity=1,delay=0.05):
+        self.sensitivity = sensitivity * polarity
+        self.running = False
+        self.bus = Bus()
+        self.delay = delay
+
+    def processing(self, values, cal_values):
+        values = [x+1 if x == 0 else x for x in values]
+        left, middle, right = values
+
+        left, middle, right = values
+        left_cal, middle_cal, right_cal = cal_values
+        left = left/left_cal
+        middle = middle/middle_cal
+        right = right/right_cal
+
+        if abs(right - left) > self.sensitivity: # means we're off the line
+            if right > left:
+                print("left")
+                turn = (left - middle) / (middle+left)
+            else: # left > right:
+                turn = (middle - right) / (middle+right)
+        else: # we're perfect
+            turn = 0
+
+        return turn
+
+    def produce_consume(self, sensor_bus, delay):
+        self.running = True
+        while self.running:
+            self.bus.write(self.processing(sensor_bus.read()))
+            time.sleep(delay)
---
+++ picarx/concun_line_folllow.py
@@ -0,0 +1,41 @@
+import time
+from concun_sense import Sensors
+from controller import Controller
+from concun_interpret import Interpreter
+from picarx_improved import Picarx
+import concurrent.futures
+
+
+def follow_line():
+    sensor = Sensors()
+    input("Press enter to calibrate grayscale, make sure all sensors are on white")
+
+    sensor.calibrate_grayscale()
+
+    # setup car things
+    interpreter = Interpreter()
+    car = Picarx()
+    controller = Controller(car)
+
+    input("Press enter to start")
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        eSensor = executor.submit(sensor.produce, sensor.bus, sensor.delay)
+        eInterpreter = executor.submit(interpreter.produce_consume, sensor.bus, interpreter.bus, interpreter.delay)
+        eController = executor.submit(controller.consume, interpreter.bus, controller.delay)
+
+    eSensor.result()
+    eInterpreter.result()
+    eController.result()
+
+    '''
+    while(True):
+        values = sensor.read()
+        print(values)
+        print([a/b for a,b in zip(values,sensor.grayscale_cal_values)])
+        controller.control(interpreter.processing(values,sensor.grayscale_cal_values))
+        car.forward(20)
+        time.sleep(0.1)
+    '''
+if __name__ == "__main__":
+    follow_line()
