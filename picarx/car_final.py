5549c16ae038c95700e714266cbecbc54b9c6d7f
CHIRAG JAIN <chirag.jain1@somaiya.edu>
2024-02-16 23:26:22 -0800

    Update car_final.py

--- picarx/car_final.py
+++ picarx/car_final.py
@@ -1,258 +1,268 @@
-import rossros as rr
-import time
-from picarx_improved import Picarx
+import logging
+from logging import DEBUG, INFO
+# set logging format
+logging_format = "%(asctime)s: %(message)s"
+logging.basicConfig(format=logging_format, level=logging.INFO, datefmt="%H:%M:%S")
+
+
+from logdecorator import log_on_start, log_on_end, log_on_error
+
 
 try:
     from robot_hat import ADC, Pin
-except ImportError:
-    from sim_robot_hat import ADC, Pin
+except ModuleNotFoundError:
+    from sim_robot_hat import Pin, ADC
 
-px = Picarx()
 
-""" First Part: Signal reading and processing functions """
+from picarx_improved import Picarx
+import rossros as rr
+import time
+from inputimeout import inputimeout
+import math
+#logging.getLogger().setLevel(logging.DEBUG)
 
-class Gray_Sensing():
+class GrayscaleSensing:
+    @log_on_end(DEBUG, "Grayscale Sensor Initialized")
+    def __init__(self, pinLeft="A0", pinMid="A1", pinRight="A2", reference = [1000]*3):
+        self.safe = True
 
-    def __init__(self):
-            self.chn_0 = ADC('A0')
-            self.chn_1 = ADC('A1')
-            self.chn_2 = ADC('A2')
-
-    def get_grayscale_data(self):
-        adc_value_list = []
-        adc_value_0 = self.chn_0.read()
-        adc_value_1 = self.chn_1.read()
-        adc_value_2 = self.chn_2.read()
-        #print(f"ADC values: {adc_value_0}, {adc_value_1}, {adc_value_2}")
-        adc_value_list.append(adc_value_0)
-        adc_value_list.append(adc_value_1)
-        adc_value_list.append(adc_value_2)
-        return adc_value_list
-
-      
-    def read(self):
-        return self.get_grayscale_data()
-
-class Gray_Interpreter():
-     
-    def __init__(self,sensitivity=0.7, polarity=-1):
-        self.sensitivity = sensitivity
-        self.polarity = polarity
-    
-    def process_sensor_data(self, readings):
-        # Interpret the readings
-        avg = sum(readings) / len(readings)
-        if self.polarity == 1:
-            interpreted_readings = [1 if (reading - avg) > self.sensitivity else 0 for reading in readings]
+        if isinstance(pinLeft,str):
+            self.chnLeft= ADC(pinLeft)
+            self.chnMid = ADC(pinMid)
+            self.chnRight = ADC(pinRight)
         else:
-            interpreted_readings = [0 if (reading - avg) > self.sensitivity else 1 for reading in readings]
+            self.chnLeft = ADC('A0')
+            self.chnMid = ADC('A1')
+            self.chnRight = ADC('A2')
+            
+        
+        self.reference(reference)
 
-        # Map the interpreted readings to a value
-        if interpreted_readings == [0, 1, 0]:
-            return 0
-        elif interpreted_readings == [0, 1, 1]:
-            return 1
-        elif interpreted_readings == [0, 0, 1]:
-            return 1
-        elif interpreted_readings == [1, 1, 0]:
-            return -1
-        elif interpreted_readings == [1, 0, 0]:
-            return -1
-        elif interpreted_readings == [1, 1, 1]:
-            return 0
-        elif interpreted_readings == [0, 0, 0]:
-            return 0
+    @log_on_start(DEBUG, "Setting Reference to: {ref}")
+    def reference(self, ref):
+        if isinstance(ref, int) or isinstance(ref, float):
+            self._reference = [ref] * 3
+        elif isinstance(ref, list) and len(ref) == 3:
+            self._reference = ref
         else:
-            return None  # Return None if the interpreted readings do not match any known pattern
+            raise TypeError("reference parameter must be \'int\', \'float\', or 1*3 list.")
 
-class Gray_Controller():
-    def __init__(self,scaling=1.0):
-        self.scaling = scaling
-
-    def control(self, value):
-        #print(f"Controller is processing value: {value}")
-        if value == 0:
-            px.set_dir_servo_angle(0)
-        elif value == 1:
-            px.set_dir_servo_angle(30*self.scaling)
-        elif value == -1:
-            px.set_dir_servo_angle(-30*self.scaling)
-        # elif value == -0.5:
-        #     px.set_dir_servo_angle(-30*self.scaling)
-        # elif value == 0.5:
-        #     px.set_dir_servo_angle(30*self.scaling)
-
-class Ult_Sensing():
-    def __init__(self, timeout=0.02):
-        self.trig = Pin('D2')
-        self.echo = Pin('D3')
-        self.timeout = timeout
-
-    def get_ultrasonic_data(self):
-        self.trig.low()
-        time.sleep(0.01)
-        self.trig.high()
-        time.sleep(0.00001)
-        self.trig.low()
-        pulse_end = 0
-        pulse_start = 0
-        timeout_start = time.time()
-        while self.echo.value()==0:
-            pulse_start = time.time()
-            if pulse_start - timeout_start > self.timeout:
-                return -1
-        while self.echo.value()==1:
-            pulse_end = time.time()
-            if pulse_end - timeout_start > self.timeout:
-                return -1
-        during = pulse_end - pulse_start
-        return during
+    #@log_on_end(DEBUG, "Grayscale Data:{result}")
+    def getGrayscaleData(self):
+        adcValues = []
+        adcValues.append(self.chnLeft.read() - self._reference[0])
+        adcValues.append(self.chnMid.read() - self._reference[1])
+        adcValues.append(self.chnRight.read() - self._reference[2])
+        return adcValues
     
-    def read(self):
-        return self.get_ultrasonic_data()
+    @log_on_end(logging.DEBUG, "Grayscale Updated")
+    def update(self):
+        return self.getGrayscaleData()
 
-class Ult_Interpreting():
-    def __init__(self):
-        pass
+class UltraSonicInerpreter():
+    def __init__(self, minDist = 0.05):
+        self.minDist = minDist
 
-    def interpret(self, data):
-        if data == -1:
-            return 1000
-        cm = round(data * 340 / 2 * 100, 2)
-        return cm
-    
-class Ult_Control():
-    def _init_(self):
-        pass
-
-    def controller(self, threshold, speed=35):
-        if threshold < 10:
-            px.forward(0)
-            px.stop
+    def update(self, rawDist):
+        if rawDist == -1:
+            return True
         else:
-            px.forward(speed)
+            return rawDist > self.minDist
 
-# class Stop_Motors():
-#     def __init__(self):
-#         pass
+class Interpretater:
+    def __init__(self, polarity = 1, sensitivity=None):
+        # polarity: 1 means line is bright, 0 means dark
+        self.sen = sensitivity
+        self.pol = polarity
+        self.safe = True
 
-#     def stop(self, signal):
-#         if signal == 1:
-#             px.stop()
-#             px.stopping_motors()
+    #@log_on_end(DEBUG, "Filtered Readings:{result}")
+    @log_on_start(DEBUG, "Filtering {rawReading}")
+    def filter(self, rawReading):
+        # returns 1 if can see line and 0 if can't for reach element of rawReading
+        avg = sum(rawReading)/len(rawReading)
+        logging.log(DEBUG, f"Filter Avg: {math.fabs(avg)}")
+        maxDiff = max(rawReading) - min(rawReading)
+        if maxDiff < 10:
+            if math.fabs(avg) < 15.0:
+                return [0,0,0]
+            return [1,1,1]
+        adj = [(x - avg) if self.pol else (avg - x) for x in rawReading]
+        logging.log(DEBUG, f"Filter Adjusted:{adj}")
+        filtered = [1 if x > 0 else 0 for x in adj]
 
-""" Second Part: Create buses for passing data """
-gray_sensor = Gray_Sensing()
-gray_interpreter = Gray_Interpreter(sensitivity=0.95, polarity=-1) #light line (-1) , dark line (1)
-gray_controller= Gray_Controller(scaling=1)
+        return filtered
 
-ult_sensor = Ult_Sensing()
-ult_interpreter = Ult_Interpreting()
-ult_controller = Ult_Control()
+    @log_on_end(DEBUG, "Interpreted Line State: {result}")
+    def interpLineState(self, filt):
+        # there are 2^3 possible outputs one of which is impossible [1,1,1]
+        # meaning 7 total possibilities
+        left, mid, right = filt
+        if(sum(filt) == 0): # can't see the line
+            return None
+        elif(mid): # we can see the line in the middle sensor
+            if(left and not right): # see mid + left
+                return -0.5
+            elif(right and not left): # see mid + right
+                return 0.5
+            elif(not right and not left): # see only mid
+                return 0.0
+            else: # see all three
+                logging.log(logging.DEBUG, f"Filted reading is {filt} and should not be possible")
+                return 0.0
+        elif(left and not right): # see only left
+            return -1.0
+        elif(not left and right): # see only right
+            return 1.0
+        else:
+            logging.log(logging.DEBUG, f"Filted reading is {filt} and should not be possible")
+            return None
 
-# stop_motors = Stop_Motors()
+    @log_on_start(logging.DEBUG, "Interpretter Loop Started")
+    def update(self, grayscaleReading):
+        filt = self.filter(grayscaleReading)
+        LS = self.interpLineState(filt)
+        logging.log(logging.DEBUG, f"Line State:{LS}")
+        return LS
+    
+class Controller:
+    def __init__(self, picar, scaling= 1.0, maxTurn = 30, pxSpeed=35):
+        self.scale = scaling
+        self.max = maxTurn
+        self.px = picar
+        self.safe = True
+        self.pxSpeed = pxSpeed
 
-# Initiate data and termination busses
-bGray_Sensing = rr.Bus(gray_sensor.read(), "Sensing bus")
-bGray_Interpreter = rr.Bus(gray_interpreter.process_sensor_data(gray_sensor.read()), "Interpreter Bus")
-bGray_Controller = rr.Bus(gray_controller.control(gray_interpreter.process_sensor_data(gray_sensor.read())), "Controller bus")
+    @log_on_start(logging.DEBUG, "Steering Angle Updated")
+    def updateSteeringAngle(self, lineState):
+        logging.log(logging.DEBUG, f"Controller got LS: {lineState}")
+        if lineState != None:
+            ang =  lineState**3 * self.max * self.scale
+            logging.log(logging.DEBUG, f"Set angle to {ang}")
+            self.px.set_dir_servo_angle(ang)
+        else:
+            logging.log(logging.DEBUG, "Controller reads None as Linestate")
 
-bUlt_Sensing = rr.Bus(ult_sensor.read(), "Sensing bus")
-bUlt_Interpreting = rr.Bus(ult_interpreter.interpret(ult_sensor.read()), "Interpreter Bus")
-bUlt_Controller = rr.Bus(ult_controller.controller(ult_interpreter.interpret(ult_sensor.read())), "Controller bus")
-
-bTerminate = rr.Bus(0, "Termination Bus")
-
-# bStop_Motors = rr.Bus(stop_motors.stop(1), "Stop Motors bus")
- 
-""" Third Part: Wrap functions into RossROS objects """
-
-# Wrap the sensing greyscale data into a producer
-readPins_gray = rr.Producer(
-    gray_sensor.get_grayscale_data,  # function that will generate data
-    bGray_Sensing,  # output data bus
-    0.001,  # delay between data generation cycles
-    bTerminate,  # bus to watch for termination signal
-    "Read pin data from greyscale module")
-
-# Wrap the multiplier function into a consumer-producer
-interpretData_gray = rr.ConsumerProducer(
-    gray_interpreter.process_sensor_data,  # function that will process data
-    bGray_Sensing,  # input data buses
-    bGray_Interpreter,  # output data bus
-    0.005,  # delay between data control cycles
-    bTerminate,  # bus to watch for termination signal
-    "Interpret grayscale data")
-
-# Wrap the multiplier function into a consumer-producer
-controlServo_gray = rr.Consumer(
-    gray_controller.control,  # function that will process data
-    bGray_Interpreter,  # input data buses
-    0.01,  # delay between data control cycles
-    bTerminate,  # bus to watch for termination signal
-    "Move direction servo")
-
-readPins_ult = rr.Producer(
-    ult_sensor.get_ultrasonic_data,  # function that will generate data
-    bUlt_Sensing,  # output data bus
-    0.002,  # delay between data generation cycles
-    bTerminate,  # bus to watch for termination signal
-    "Read pin data from ultrasonic module")
-
-interpretData_ult = rr.ConsumerProducer(
-    ult_interpreter.interpret,  # function that will process data
-    bUlt_Sensing,  # input data buses
-    bUlt_Interpreting,  # output data bus
-    0.008,  # delay between data control cycles
-    bTerminate,  # bus to watch for termination signal
-    "Interpret ultrasonic data")
-
-controlServo_ult = rr.Consumer(
-    ult_controller.controller,  # function that will process data
-    bUlt_Interpreting,  # input data buses
-    0.02,  # delay between data control cycles
-    bTerminate,  # bus to watch for termination signal
-    "Move direction servo")
-
-# stopMotors = rr.Consumer(
-#     stop_motors.stop,  # function that will process data
-#     bTerminate,  # input data buses
-#     0.01,  # delay between data control cycles
-#     bTerminate,  # bus to watch for termination signal
-#     "Stop Motors")
+    def updateDriving(self, isClearAhead):
+        if isClearAhead:
+            self.px.forward(self.pxSpeed)
+        else:
+            self.px.stop()
 
 
-""" Fourth Part: Create RossROS Printer and Timer objects """
+logging.getLogger().setLevel(logging.INFO)
+if __name__=="__main__":
+    # some constants and determined through careful testing and not made up
+    ref = [31.28, 37.29, 36.66] 
+    speed = 35
+    maxAngle = 30
+    polarity = 1.0
+    scaling = 1.0
+    avoidObs = True
+    updateFreq = 1.0/20.0
+    usUpdateFreq = 1.0/4.0
+    timeout = 3.0
 
-# Make a printer that returns the most recent wave and product values
-printBuses = rr.Printer(
-    (bGray_Sensing, bGray_Interpreter, bTerminate),  # input data buses
-    # bMultiplied,      # input data buses
-    0.25,  # delay between printing cycles
-    bTerminate,  # bus to watch for termination signal
-    "Print raw and derived data",  # Name of printer
-    "Data bus readings are: ")  # Prefix for output
+    # define busses
+    gsSensorBus = rr.Bus([0.0]*3, "Grayscale Raw Data Bus")
+    lineStateBus = rr.Bus([0]*3, "Line State Bus")
+    if avoidObs:
+        usSensorBus = rr.Bus(False, "Ultrasonic raw sensor reading")
+        isClearBus = rr.Bus(False, "Interped Ultrasonic Sensor")
+    termBus = rr.Bus(0, "Termination Bus")
 
-# Make a timer (a special kind of producer) that turns on the termination
-# bus when it triggers
-terminationTimer = rr.Timer(
-    bTerminate,  # Output data bus
-    15,  # Duration
-    0.01,  # Delay between checking for termination time
-    bTerminate,  # Bus to check for termination signal
-    "Termination timer")  # Name of this timer
 
-""" Fifth Part: Concurrent execution """
+    # define worker classes
+    px = Picarx()
+    grayscale = GrayscaleSensing("A0", "A1", "A2",ref)
+    interp = Interpretater(polarity=polarity)
+    cont = Controller(px, scaling, maxAngle, speed)
+    if avoidObs:
+        ultraInterp = UltraSonicInerpreter()
 
-# Create a list of producer-consumers to execute concurrently
-producer_consumer_list = [readPins_gray,
-                          interpretData_gray,
-                          controlServo_gray,
-                          readPins_ult,
-                          interpretData_ult,
-                          controlServo_ult,
-                          printBuses,
-                          terminationTimer] # add printBuses, if using printer
 
-# Execute the list of producer-consumers concurrently
-rr.runConcurrently(producer_consumer_list)
+
+    # Wrap the classes into consumer/producer classes
+    readGS = rr.Producer(
+        grayscale.update,
+        gsSensorBus,
+        updateFreq,
+        termBus,
+        "Read raw grayscale sensor data"
+    )
+
+    if  avoidObs:
+        readUS = rr.Producer(
+            px.ultrasonic.read,
+            usSensorBus,
+            usUpdateFreq,
+            termBus,
+            "Read raw ultrasonic sensor data"
+        )
+
+    interpLS = rr.ConsumerProducer(
+        interp.update,
+        gsSensorBus,
+        lineStateBus,
+        updateFreq,
+        termBus,
+        "Grayscale Data Interping Line State"
+    )
+
+    if avoidObs:
+        interpUS = rr.ConsumerProducer(
+            ultraInterp.update,
+            usSensorBus,
+            isClearBus,
+            usUpdateFreq,
+            termBus,
+            "Ultrasonic sensor reading to is clear"
+        )
+
+
+    controlSA = rr.Consumer(
+        cont.updateSteeringAngle,
+        lineStateBus,
+        updateFreq,
+        termBus,
+        "Line state to steering control"
+    )
+
+    if avoidObs:
+        controlF = rr.Consumer(
+            cont.updateDriving,
+            isClearBus,
+            usUpdateFreq,
+            termBus,
+            "Is clear to forward driving"
+        )
+    
+    termTimer = rr.Timer(
+        termBus,
+        timeout,
+        updateFreq,
+        termBus,
+        "Termination Timer"
+    )
+
+    if avoidObs:
+        printList = (gsSensorBus, lineStateBus, usSensorBus, isClearBus, termBus)
+    else:
+        printList = (gsSensorBus, lineStateBus, termBus)
+
+    printBuses = rr.Printer(
+        printList,
+        #(gsSensorBus, termBus),
+        0.25,
+        termBus,
+        "Print raw and derived data",
+        "Data bus readings: "
+    )
+    pc_list = [readGS, interpLS, controlSA, printBuses]
+    if avoidObs:
+        for x in (readGS, interpUS, controlF):
+            pc_list.append(x)
+
+    pc_list.append(termTimer)
+
+    rr.runConcurrently(pc_list)
